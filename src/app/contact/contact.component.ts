fb838c62f15aa9161b0b70be39a4b6dfdb4aaee8
MirkanPolat <Mirkanpolat@outlook.de>
2025-05-27 16:55:46 +0200

    adding like function and contact routerlink

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+
 @Component({
-  selector: 'app-root',
+  selector: 'app-contact',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.scss'
 })
-export class AppComponent {
-  title = 'insta-clone';
+export class ContactComponent {
+
 }
--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 interface Post {
   username: string;
   img: string;
   likes: number;
+  isliked: boolean;
 }
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
@@ -20,28 +22,30 @@ export class MainPageComponent {
     {
       username: 'user1',
       img: 'banana.jpg', 
-      likes: 0 
+      likes: 4,
+      isliked: true
     },
     {
       username: 'user2',
       img: 'currant.jpg',
-      likes: 0
+      likes: 62,
+      isliked: true
     },
     {
       username: 'user3',
-      img: 'heart-black.png',
-      likes: 0
-    },
-    {
-      username: 'user4',
-      img: 'heart-red.png',
-      likes: 0
-    },
-    {
-      username: 'user5',
       img: 'orange.jpg',
-      likes: 0
+      likes: 8,
+      isliked: true 
     }
   ]
 
+  toggleLike(post: Post) {
+    if (post.isliked) {
+      post.isliked = !post.isliked
+      post.likes -= 1
+    } else {
+      post.isliked = !post.isliked
+      post.likes += 1
+    }
+  }
 }
